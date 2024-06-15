9f7612f4f519236484c16e13eef818e22ed47bf2
mccullen <mccullen_j@lynchburg.edu>
2024-06-15 14:47:35 -0400

    Add gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -55,6 +56,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <GoogleTagManager gtmId="GTM-MLJ88KND" />
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
